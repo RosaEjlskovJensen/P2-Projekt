e8b73954be691a5b08013fb1a79360a13f8503e2
WassawRoki <Casper.roskaer@gmail.com>
2020-02-20 08:54:34 +0100

    Burger virker

--- burgermenu.php
+++ burgermenu.php
@@ -12,88 +12,23 @@
       <link rel="stylesheet" href="stylesheet.css">
     <!-- Linker til normalize der styre font størelser på små skærme -->
     <link rel="stylesheet" href="normalize.css">
-
-<style>
-nav{
-  background: #000000;
-  color: #ffffff;
-  list-style: none;
-  clear: both;
-    margin-right: 17.33%;
-}
-nav li{
-display: inline-block;
-margin: 10px;
-}
-body{
-  margin:0;
-}
-.burger div{
-  width: 35px;
-  height: 5px;
-  background: #ffffff;
-  margin: 6px 0;
-}
-#burger{
-  float: left;
-  visibility:hidden;
-  margin: 5px;
-}
-
-@media screen and (max-width:700px){
-  #burger{
-  visibility: visible;
-}
-.burger{
-display: block;
-height: 40px;
-width: 30px;
-float: right;
-cursor: pointer;
-}
-nav{
-  list-style: none;
-  overflow: hidden;
-  min-height: 40px;
-  margin: 0px auto;
-}
-nav li{
-  display: none;
-}
-
-nav li.open{
-float: none;
-text-align: left;
-margin:0;
-visibility: visible;
-display: block;
-text-align: center;
-padding-top: 40px;
-width: 100%;
-height: auto;
-}
-}
-</style>
-
-
   </head>
   <body>
-    <div class="offset-by-two columns eight columns">
-    <nav>
-      <!--Navigation burger-->
+    <nav class="u-full-width">
+      <!--Navigation burger start-->
     <div id="burger" class="burger">
       <div></div>
       <div></div>
       <div></div>
     </div>
-    
-    <br>	
-        <li>page1</li>
-        <li>page2</li>
-        <li>page3</li>
-        <li>page4</li>
-        <li>page5</li>
+    <!--Navigation burger slut-->
+        <li><a href="nyhedsbrev.php" class="button">nyhedsbrev</a></li>
+        <li><a href="send_form_email.php" class="button">Kontakt</a></li>
+        <li><a href="Blog_Page.php" class="button">Blogs</a></li>
+        <li><a href="My_Pictures.php" class="button">Bestil dine billeder her</a></li>
+        <li><a href="Admin side/Loginside.php" class="button">Admin Login</a></li>
+
     </nav>
-</div>
+ 
   </body>
 </html>
--- index.php
+++ index.php
@@ -1,28 +1,34 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<!-- Linker til Skeleton -->
-<link rel="stylesheet" href="stylesheet.css">
-<!-- Linker til normalize der styre font størelser på små skærme -->
-<link rel="stylesheet" href="normalize.css">
-<!-- Dette link er ikonet der er i ens browser tab -->
-<link rel="icon" type="image/png" href="INDSET IKON HER">
-<title>Index</title>
-</head>
+  <head>
+    <meta charset="utf-8">
+    <title>Burgermenu</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="burgermenujs.js"></script>
+ <!-- Dette link er ikonet der er i ens browser tab -->
+    <link rel="icon" type="image/png" href="INDSET IKON HER">
+    <title>Kontakt</title>
+    <!-- Linker til Skeleton -->
+      <link rel="stylesheet" href="stylesheet.css">
+    <!-- Linker til normalize der styre font størelser på små skærme -->
+    <link rel="stylesheet" href="normalize.css">
+  </head>
+  <body>
+    <nav class="u-full-width">
+      <!--Navigation burger start-->
+    <div id="burger" class="burger">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <!--Navigation burger slut-->
+        <li><a href="nyhedsbrev.php" class="button">nyhedsbrev</a></li>
+        <li><a href="send_form_email.php" class="button">Kontakt</a></li>
+        <li><a href="Blog_Page.php" class="button">Blogs</a></li>
+        <li><a href="My_Pictures.php" class="button">Bestil dine billeder her</a></li>
+        <li><a href="Admin side/Loginside.php" class="button">Admin Login</a></li>
 
-<body>
-  
-<div class="row">
-	<div class="greytopbar">
-		<a href="Admin side/Loginside.php" class="button-primary u-pull-right">Admin Login</a>
-		<a href="nyhedsbrev.php" class="button-primary">nyhedsbrev</a>	
-		<a href="send_form_email.php" class="button-primary">Kontakt</a> 
-		<a href="Blog_Page.php" class="button-primary">Blogs</a> 
-		<a href="My_Pictures.php" class="button-primary">Bestil dine billeder her</a>
-
-	</div>
-</div>
-
-</body>
+    </nav>
+ 
+  </body>
 </html>
