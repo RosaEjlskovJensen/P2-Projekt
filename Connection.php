13ed72562dab7eb1cb74668b71759ab391fef4c4
WassawRoki <Casper.roskaer@gmail.com>
2020-02-20 12:38:19 +0100

    fixet connections

--- Connection.php
+++ Connection.php
@@ -1,6 +1,6 @@
 <?php
 //database connection
-$connection = mysqli_connect('localhost','root','','newsletter');
+$connection = mysqli_connect('localhost','root','','data');
 if(!$connection){
 die("cannot connect to database".mysqli_connect_error());
 }
--- Connection_blog.php
+++
@@ -1,7 +0,0 @@
-<?php
-//database connection
-$connection = mysqli_connect('localhost','root','','blog');
-if(!$connection){
-die("cannot connect to database".mysqli_connect_error());
-}
-?>
