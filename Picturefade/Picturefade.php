504ff84ef9849d879553b5f50af1f1b07b8297eb
daprior <57490855+daprior@users.noreply.github.com>
2020-02-22 20:27:10 +0100

    picturefade

---
+++ Picturefade/Picturefade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+ <meta charset="utf-8">
+    <title>Picturefade</title>
+ <!-- Dette link er ikonet der er i ens browser tab -->
+    <link rel="icon" type="image/png" href="INDSET IKON HER">
+    <title>Kontakt</title>
+    <!-- Linker til Skeleton -->
+      <link rel="stylesheet" href="Picturefade.css">
+    <!-- Linker til normalize der styre font størelser på små skærme -->
+    <link rel="stylesheet" href="normalize.css">
+</head>
+<body>
+<!-- Koden der indsætter billedet og laver divs dertil. -->
+
+<div class="container">
+  <img src="%C3%98l-klippekort.png" alt="Avatar" class="image" style="width:100%">
+  <div class="middle">
+    <div class="text">Eksempel - Link</div>
+  </div>
+</div>
+  
+</body>
+</html>
