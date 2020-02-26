8a2ee9335dcc802b4714821905a72fa599da879e
WassawRoki <Casper.roskaer@gmail.com>
2020-02-26 12:02:42 +0100

    Integreret frost picture i skeleton
     50% done

--- Picturefade/Picturefade.php
+++ Picturefade/Picturefade.php
@@ -8,19 +8,18 @@
     <link rel="icon" type="image/png" href="INDSET IKON HER">
     <title>Kontakt</title>
     <!-- Linker til Skeleton -->
-      <link rel="stylesheet" href="Picturefade.css">
+      <link rel="stylesheet" href="../stylesheet.css">
     <!-- Linker til normalize der styre font størelser på små skærme -->
     <link rel="stylesheet" href="normalize.css">
 </head>
 <body>
 <!-- Koden der indsætter billedet og laver divs dertil. -->
-
-<div class="container">
-  <img src="%C3%98l-klippekort.png" alt="Avatar" class="image" style="width:100%">
-  <div class="middle">
-    <div class="text">Eksempel - Link</div>
-  </div>
-</div>
+<div class="row five columns offset-by-one-third">
+	<div class="frost_container">
+	  <img src="../Billeder/Test_Picture.png" alt="Avatar" class="u-full-width frost_image">
+		<div class="two columns frost_text">Link Text</div>
+	</div>
+ </div>
   
 </body>
 </html>
