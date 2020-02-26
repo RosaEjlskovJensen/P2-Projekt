7b561f89db6129ae7bd61ddeab8e06ccc378fa82
Mohamed Omar <57672189+mo9000@users.noreply.github.com>
2020-02-26 12:04:31 +0100

    Homepage-Layout

---
+++ Homepage-Layout.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+	 <!-- Dette link er ikonet der er i ens browser tab -->
+    <link rel="icon" type="image/png" href="file:///Macintosh HD/Users/mohamedomar/Desktop/INDSET IKON HER">
+    <title>Kontakt</title>
+    <!-- Linker til Skeleton -->
+      <link rel="stylesheet" href="file:///Macintosh HD/Users/mohamedomar/Desktop/stylesheet.css">
+    <!-- Linker til normalize der styre font størelser på små skærme -->
+    <link rel="stylesheet" href="file:///Macintosh HD/Users/mohamedomar/Desktop/normalize.css">
+<title>Website Layout with Photo Gallery</title>
+<style>
+	
+	.Billeder{
+		margin: :5px;
+		border: 1px solid #6F6F6F;
+		float: left;
+		width: 390px;
+		display: grid;
+	}
+	
+	
+	.Billeder img{
+		width: 100%;
+		max-height: inherit;
+		object-position: left;
+		
+	}
+	.desc{
+		padding: 15px;
+		text-align: center;
+		
+	}
+	
+</style>
+</head>
+
+<body>
+<div class="twelve columns">
+<div class="u-full-width">
+	<img src="Billeder/Photostudio.png">
+	<div class="desc"> Add a description of the image here<hr></div>
+	</div>
+</div>
+	
+	
+<br>
+
+<div> class="row"</div>
+<div class="Billeder">
+	<img src="Billeder/baby.png">
+	<div class="desc"> Add a description of the image here</div>
+	
+	 </div>
+	
+	
+<div class="Billeder">
+	<img src="Billeder/pregnant.png">
+	<div class="desc"> Add a description of the image here</div>
+	
+	</div>
+	
+	<br>
+	
+	<div class="Billeder">
+	<img src="Billeder/Weddingpic.png">
+	<div class="desc"> Add a description of the image here</div>
+	
+	 </div>
+	
+<div class="Billeder">
+	<img src="Billeder/pets.png">
+	<div class="desc"> Add a description of the image here</div>
+	
+	 </div>
+	
+	
+	
+	
+</body>
+</html>
