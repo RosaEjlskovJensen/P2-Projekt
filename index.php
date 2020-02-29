005f53563ef8ba472f1c7b80664cb80e52d7ca76
Rosa Ejlskov Jensen <Rosa.jensen@rocketmail.com>
2020-02-29 20:17:12 +0100

    Index

--- index.php
+++ index.php
@@ -24,8 +24,7 @@
     .showcase_picture{
       max-width: 100%;
       height: 91.5vh;
-      position: relative;
-      top: 0;
+      width: 100vw;
     }
 
     .img_logo{
@@ -64,39 +63,11 @@
       text-align: center;
     }
 
-    /* Next & previous buttons */
-    .prev, .next {
-      cursor: pointer;
-      position: absolute;
-      top: 50%;
-      width: auto;
-      margin-top: -30px;
-      padding: 16px;
-      color: #888;
-      font-weight: bold;
-      font-size: 20px;
-      border-radius: 0 3px 3px 0;
-      user-select: none;
-    }
-
-    /* Position the "next button" to the right */
-    .next {
-      position: absolute;
-      right: 0;
-      border-radius: 3px 0 0 3px;
-    }
-
-    /* On hover, add a black background color with a little bit see-through */
-    .prev:hover, .next:hover {
-      background-color: rgba(0,0,0,0.8);
-      color: white;
-    }
-
     /* The dot/bullet/indicator container */
     .dot-container {
       text-align: center;
       padding: 20px;
-      background: #ddd;
+      background: #e3e3dd;
     }
 
     /* The dots/bullets/indicators */
@@ -105,7 +76,7 @@
       height: 15px;
       width: 15px;
       margin: 0 2px;
-      background-color: #bbb;
+      background-color: #234946;
       border-radius: 50%;
       display: inline-block;
       transition: background-color 0.6s ease;
@@ -113,7 +84,7 @@
 
     /* Add a background color to the active dot/circle */
     .active, .dot:hover {
-      background-color: #717171;
+      background-color: #2D2926;
     }
 
     /* Add an italic font style to all quotes */
@@ -159,18 +130,12 @@
             Blog:      <li><a href="Blog_Page.php" class="button">Blog</a></li>
             Booking:   <li><a href="Booking.php" class="button">Booking</a></li>
             Kontakt:   <li><a href="Kontakt.php" class="button">Kontakt</a></li>
-          
-        <li><a href="nyhedsbrev.php" class="button">nyhedsbrev</a></li>
-        <li><a href="send_form_email.php" class="button">Kontakt</a></li>
-        <li><a href="Blog_Page.php" class="button">Blogs</a></li>
-        <li><a href="My_Pictures.php" class="button">Bestil dine billeder her</a></li>
-        <li><a href="Admin side/Loginside.php" class="button">Admin Login</a></li>
-      -->
+        -->
 
       <!-- Showcase Sektion  -->
       <section class="section_showcase">
         <div class="u-full-width">
-          <img class="showcase_picture" src="billeder/FTB1.jpg" width="100%">
+          <img class="showcase_picture" src="billeder/AmalieSandgaardPhotography_LOGO.png" width="100%">
         </div>
       </section>
 
@@ -249,80 +214,32 @@
         <section class="section_galleri_row1">
           <div class="container">
             <div class="row">
-              <div class="six columns">
+              <div class="four columns">
                 <a href="send_form_email.php"><img class="link_picture" src="billeder/FBBBB1.jpg"></a>
               </div>
-              <div class="six columns">
+              <div class="four columns">
                 <a href="send_form_email.php"><img class="link_picture" src="billeder/FBBB1.jpg"></a>
               </div>
-            </div>
-          </div>     
-        </section>
-        <section class="section_galleri_row2">
-          <div class="container">
-            <div class="row">
-              <div class="six columns">
+              <div class="four columns">
                 <a href="send_form_email.php"><img class="link_picture" src="billeder/FFB1.jpg"></a>
               </div>
-              <div class="six columns">
+            </div>
+            <div class="row">
+              <div class="four columns">
                 <a href="send_form_email.php"><img class="link_picture" src="billeder/FKB1.jpg"></a>
               </div>
-            </div>
-          </div>     
-        </section>
-        <section class="section_galleri_row3">
-          <div class="container">
-            <div class="row">
-              <div class="six columns">
+              <div class="four columns">
                 <a href="send_form_email.php"><img class="link_picture" src="billeder/FBB1.jpg"></a>
               </div>
-              <div class="six columns">
-                <a href="send_form_email.php"><img class="link_picture" src="billeder/FBB1.jpg"></a>
+              <div class="four columns">
+                <a href="send_form_email.php"><img class="link_picture" src="billeder/FBBB1.jpg"></a>
               </div>
-            </div>
+            </div>  
           </div>     
         </section>
 
-   <!--
 
-  <section class="section_galleri">
-    <center><div><h1>Hvem fotografer jeg?</h1></div></center>
-    <div class="container">
-      <div class="row">
-        <div class="six columns">
-          <a href="send_form_email.php"><img class="showcase_picture" src="billeder/Test_Picture.png"></a>
-        </div>
-        <div class="six columns">
-          <a href="#"><img class="showcase_picture" src="billeder/Test_Picture.png"></a>
-        </div>
-      </div>
-    </div>
-    <br>
-    <div class="container">
-      <div class="row">
-        <div class="six columns">
-          <a href="#"><img class="showcase_picture" src="billeder/Test_Picture.png"></a>
-        </div>
-        <div class="six columns">
-          <a href="#"><img class="showcase_picture" src="billeder/Test_Picture.png"></a>
-        </div>
-      </div>
-    </div>
-    <br>
-    <div class="container">
-      <div class="row">
-        <div class="six columns">
-          <a href="#"><img class="showcase_picture" src="billeder/Test_Picture.png"></a>
-        </div>
-        <div class="six columns">
-          <a href="#"><img class="showcase_picture" src="billeder/Test_Picture.png"></a>
-        </div>
-      </div>
-    </div>
- </section>
-
--->
-
+<br>
 <br>
 
 <!-- Google Maps Sektion-->
@@ -336,31 +253,9 @@
   </div>
 </section> 
 
-<!-- Test Sektion
+<br>
 
-<section class="section_test">
-  <div class="container">
-    <div class="row">
-      <div class="three columns">
-        <button class="btn btn1" type="button">Testing!</button>
-      </div>
-      <div class="three columns">
-        <button class="btn btn2" type="button">Testing!</button>
-      </div>
-      <div class="three columns">
-        <button class="btn btn3" type="button">Testing!</button>
-      </div>
-      <div class="three columns">
-        <button class="btn btn4" type="button">Testing!</button>
-      </div>
-    </div>
-  </div>
-</section>
-
--->
-
-
-<!-- Slut Sektion -->
+<!-- Info Sektion -->
 <section class="section_footer">
   <div class="u-full-width">
     <center>
