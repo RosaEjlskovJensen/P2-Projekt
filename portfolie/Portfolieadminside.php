124b4ec79707d107876b6ff46142dc4fdfce1f21
daprior <57490855+daprior@users.noreply.github.com>
2020-03-02 15:03:35 +0100

    Portfolie
    hey

---
+++ portfolie/Portfolieadminside.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Portfolie</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="..//burgermenujs.js"></script>
+ <!-- Dette link er ikonet der er i ens browser tab -->
+    <link rel="icon" type="image/png" href="INDSET IKON HER">
+    <!-- Linker til Skeleton -->    
+    <!-- <link rel="stylesheet" href="..//stylesheet.css"> -->
+    <!-- Linker til normalize der styre font størelser på små skærme -->
+    <link rel="stylesheet" href="..//normalize.css">
+  <!-- Linker til Fontawsome -->
+  <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Linker til Stylesheet -->
+  <link rel="stylesheet" href="..//stylesheet.css">
+</head>
+
+    <body>
+        
+    <div class="row">
+		<div class="eight columns">	
+			<div class="">
+				<h2> Vælg kategori <h2>
+				<a href="baby.php" class="button-primary">Baby</a>
+				<a href="bryllup.php" class="button-primary">Bryllup</a>
+				<a href="b%C3%B8rn.php" class="button-primary">Børn</a>
+                <a href="familie.php" class="button-primary">familie</a>
+                <a href="gravid.php" class="button-primary">gravid</a>
+                <a href="konfirmation.php" class="button-primary">konfirmation</a>
+                <a href="../Admin%2520side/Admin.php" class="button-primary">tilbage</a>
+                <a href="../index.php" class="button-primary">til forside</a>
+			</div>
+		</div>
+	</div>
+  </body>
+    
+</html>
---
+++ portfolie/Portfoliebaby.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>  
+
+<html>  
+ <head>  
+  <title>Portfolie baby</title>  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="burgermenujs.js"></script>
+  <!-- Dette link er ikonet der er i ens browser tab -->
+  <link rel="icon" type="image/png" href="billeder/asp.png">
+  <!-- Linker til Skeleton -->
+
+  <!-- Linker til Fontawsome -->
+
+  <!-- Linker til Stylesheet -->
+ 
+ </head>  
+    <body>
+
+
+<center>
+         <h1> billeder </h1>
+</center>
+
+<?php
+//database connection
+$connection = mysqli_connect('localhost','root','root','data');
+if(!$connection){
+die("cannot connect to database".mysqli_connect_error());
+}
+?>
+    <?php
+ {
+  $query = "SELECT * FROM baby ORDER BY id DESC";
+  $result = mysqli_query($connection, $query);
+  
+  while($row = mysqli_fetch_array($result))
+  {
+   $output .= '
+   
+    <div>
+      <img src="data:image/jpeg;base64,'.base64_encode($row['name'] ).'" height="200" width="200" />
+    </div>
+   ';
+  }
+  echo $output;
+ }
+    ?>
+
+    
+  </body>
+</html>
