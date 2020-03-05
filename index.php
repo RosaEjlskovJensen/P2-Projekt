5d81bcf3428ae409f00c9c97be060f96fbf39734
Rosa Ejlskov Jensen <Rosa.jensen@rocketmail.com>
2020-03-05 20:46:27 +0100

    Index v4 + clean css

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 	<title>Amalie Sandgaard | Photography</title>
 	<!-- ajax/jquery -->
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <!-- -->
+  <script src="mySlides.js"></script>
 	<!-- Dette link er ikonet der er i ens browser tab -->
   	<link rel="icon" type="image/png" href="billeder/asp.png">
   	<!-- Linker til Skeleton -->
@@ -39,12 +41,6 @@
     </div>
   </section>
   <br>
-
- <!--Testimonials -->
- <section class="section_testimonials">
- 
- </section>
-
 
  <!--Galleri -->
   <section class="section_galleri">
@@ -98,6 +94,102 @@
   	</div>  
   </section>
   <br>
+
+
+  <!--Testimonials -->
+  <section class="section_testimonials">
+    <div class="container">
+      <div class="row">
+        <div class="u-full-width">
+          <center><h6>See hvad kunderne siger (arrow down)</h6></center>
+        </div>
+      <div class="row">
+        <div class="u-full-width">
+          <div class="slideshow_container">
+            <div class="slides">
+              <div class="mySlides">
+                <center><q>Jeg er glad for at få taget nogle billeder hos Amalie da hun tager imod en med et smil og åbentsind. Hun søger for at man hele tiden er i centrum og er klar på nogle nye idéer.</q><br>
+                <p class="author">- Tommy Rank</p></center>
+              </div>
+              <div class="mySlides">
+                <center><q>Utrolig sød og behagelig ung pige ❤️ hun har en fantastisk tålmodighed og kan bevarede overblikket til min datters konfirmation hvor alle vi andre rendte forvirret rundt en stor tak for de helt fantastiske billeder og din måde at være på - kan helt klart anbefale dig og vil også gøre det til andre - du er en stjerne. ❤️❤️❤️</q><br>
+                <p class="author">- Lene Tranbjerg</p></center>
+              </div>
+              <div class="mySlides">
+                <center><q>Jeg har lige fået taget billeder af min søn på tre år - og det var en rigtig god oplevelse. Amalie havde med det samme en god kontakt med min søn, så vi fik taget nogle rigtig gode billeder, som viser alle facetterne hos min søn. Jeg kan helt klart anbefale Amalie.</q><br>
+                <p class="author">- Annemette Sørensen</p></center>
+              </div>
+              <div class="mySlides">
+                <center><q>Vi havde Amalie til at tage fotos til vores bryllup - det kom der mange rigtig gode billeder ud af. Kan klart anbefales.</q><br>
+                <p class="author">- Morten Kierkegaard</p></center>
+              </div>
+              <div class="mySlides">
+                <center><q>Vi blev hurtigt fascineret af de billeder Amalie tager og besluttede, at det var hende som skulle forevige vores søn’s store dag. Vi bookede 3 timers fotosession, og fik alle vores ønsker indfriet. Amalie er samarbejdsvillig, fyldt med gode ideer, og fanger de gode øjeblikke. Samme dag hun havde taget billederne, fik vi et “sneak peak” om aftenen, og næste dag modtog vi den færdige billedpakke. Vi er fuldt tilfredse, med det arbejde Amalie har udført og kan varmt anbefale hende. Vi er ikke i tvivl om, at det også er Amalie, vi vælger næste gang, vi får brug for en fotograf.</q><br>
+                <p class="author">- Mette Sørensen</p></center>
+              </div>
+              <div class="mySlides">
+                <center><q>Amalie var hjemme hos os og vi fik nogle rigtigt gode billeder,som vi glæder os til at kunne give til familien.</q><br>
+                <p class="author">- Hanne Theilmann Petersen</p></center>
+              </div>
+              <div class="mySlides">
+                <center><q>Vi fik taget billeder af vores datter på 4 uger og selvom hun var lidt urolig, tog Amalie det i stiv arm og fik taget nogle super gode billeder. :-) Super sød og tålmodig pige, som kan sit kram. :-)</q><br>
+                <p class="author">- Annedorte Khouri </p></center>
+              </div>
+              <div class="mySlides">
+                <center><q>Super sød pige med en masse tålmodighed - selv når man har en knap 2 årige med, som absolut ikke vil det vi vil Vi fik nogle fantastiske billeder fra den dag, så kan VARMT ANBEFALES! ⭐⭐⭐⭐⭐</q><br>
+                <p class="author">- Annika Grandt Madsen</p></center>
+              </div>
+              <div class="mySlides">
+                <center><q>Amalie sprang med kort varsel til, så vi kunne få taget familiebilleder i det fri med vores 2-årige. Flotte billeder, god efterbearbejdning og masser af tålmodighed med den lille on location. Kan varmt anbefales.</q><br>
+                <p class="author">- Louise Hjort Nielsen</p></center>
+              </div>
+              <div class="mySlides">
+                <center><q>Amalie fotograferede til vores datter Yasmin´s konfirmation her i maj 2017. Amalie var fantastisk til at få nogle fantastiske billeder både med konfirmanden, men også med de øvrige gæster. Med hendes vindende væsen og positive tilgang - så vandt hun hurtigt alles opmærksomhed på en positiv måde. Vi har fået nogle helt fantastiske billeder fra en fantastisk dag og vil varmt anbefale Amalie til andre.</q><br>
+                <p class="author">- Tommy Nielsen</p></center>
+              </div>
+            <div class="dot-container">
+              <span class="dot" onclick="currentSlide(1)"></span> 
+              <span class="dot" onclick="currentSlide(2)"></span> 
+              <span class="dot" onclick="currentSlide(3)"></span> 
+              <span class="dot" onclick="currentSlide(4)"></span>
+              <span class="dot" onclick="currentSlide(5)"></span> 
+              <span class="dot" onclick="currentSlide(6)"></span> 
+              <span class="dot" onclick="currentSlide(7)"></span> 
+              <span class="dot" onclick="currentSlide(8)"></span> 
+              <span class="dot" onclick="currentSlide(9)"></span> 
+              <span class="dot" onclick="currentSlide(10)"></span>  
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <br>
+
+ <!-- Booking -->
+ <section class="section_booking">
+   <div class="u-full-width">
+     <div class="container">
+      <center>
+        <div class="row">
+          <div class="u-full-width">
+           <h5>Mangler du en fotograf?</h5>
+           <p>Helt fra barnsben, har jeg været, grebet om ideen at skulle forevige øjeblikke, mennesker, dyr, stemninger mm. Der gik mange år, fra da jeg fik det første analog kamera i ti års alderen, til at jeg fik købt mit første DSLR kamera i 2010, da jeg blev komfimreret. Siden da, har jeg bygget mere og mere på min viden som fotograf. Jeg har blandt andet været i Herning Fotoklub, hvor jeg lærte om blænde, lukketid, hvidbalance, iso og så videre.
+           </p>
+           <div class="row">
+            <div class="six columns">
+              <form action="#"><button type="submit" class="btn btn2">Se Datoer</button></form> 
+            </div>
+            <div class="six columns">
+              <form action="#"><button type="submit" class="btn btn2">Se Priser</button></form> 
+            </div>
+          </center>   
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
 
  <!-- Ínformation -->
   <section class="section_maps">
