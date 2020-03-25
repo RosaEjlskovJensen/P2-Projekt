68c6b84beeb9da19a663a1100ab98cfd68e57a9d
daprior <57490855+daprior@users.noreply.github.com>
2020-03-25 18:23:01 +0100

    kontakt

---
+++ Kontakt/Kontakt.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Amalie Sandgaard | Photography</title>
+	<!-- ajax/jquery -->
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <!-- -->
+  <script src="mySlides.js"></script>
+	<!-- Dette link er ikonet der er i ens browser tab -->
+  	<link rel="icon" type="image/png" href="../Billeder/asp.png">
+  	<!-- Linker til Skeleton -->
+  	<!-- Linker til Fontawsome -->
+  	<script src="https://kit.fontawesome.com/600e3ecdcb.js" crossorigin="anonymous"></script>
+  	<!-- Stylesheets -->
+	<link rel="stylesheet" type="text/css" href="../main.css">
+</head>
+
+<style>
+    .lokation {
+        float: right;
+        padding-right:10%;
+        padding-top: 5%;
+}
+    .informationer {
+        float: right;
+        width: 50%;
+        padding-left: 20px;
+        display: inline-block;
+}
+    .kontaktinformation {
+        display: inline-block;
+        padding-left: 10%;
+}
+    
+    .kontaktinfvenstre {
+        display: inline-block;
+        float:left;
+}
+    
+    .kontaktinfvenstre {
+        display: inline-block;
+        float:left;
+}
+    .kontaktbillede {
+        display: inline-block;
+        float:left;
+        padding-left: 20px;
+        width: 50px;
+}
+    .kontaktikon {
+        width: 120px;
+        height: 120px;
+}
+</style>
+
+<body>
+
+<!-- Navigation bar -->
+  <nav class="nav">
+    <a href="index.php"><img class="img_logo" src="../billeder/AmalieSandgaardPhotography_LOGO.png"></a>
+    <input type="checkbox" id="menu_btn" class="menu_btn" />
+    <label class="menu_icon" for="menu_btn"><span class="nav_icon"></span></label>
+    <ul class="menu">
+      <div class="nav_dropdown">
+        <button class="btn_dropdown">Portfolio <i class="fas fa-chevron-down"></i></button>
+          <div class="dropdown-content">
+          <li><a href="My_Pictures.php" class="button">Baby</a></li>
+          <li><a href="My_Pictures.php" class="button">Børn</a></li>
+          <li><a href="My_Pictures.php" class="button">Familie</a></li>
+          <li><a href="My_Pictures.php" class="button">Konfirmation</a></li>
+          <li><a href="My_Pictures.php" class="button">Bryllup</a></li>
+          <li><a href="My_Pictures.php" class="button">Gravid</a></li>
+            </div>
+            </div>
+      <div class="nav_dropdown">
+        <button class="btn_dropdown">Info <i class="fas fa-chevron-down"></i></button>
+          <div class="dropdown-content">
+          <li><a href="Om_Mig.php" class="button">Om Mig</a></li>
+          <li><a href="Blog_Page.php" class="button">Blog</a></li>
+          <li><a href="nyhedsbrev.php" class="button">Nyheder</a></li>
+          <li><a href="Blog_Page.php" class="button">Anmeldeser</a></li>
+            </div>
+            </div>
+      <div class="nav_dropdown">
+        <button class="btn_dropdown">Priser <i class="fas fa-chevron-down"></i></button>
+          <div class="dropdown-content">
+          <li><a href="Admin side/Loginside.php" class="button">Pakker</a></li>
+          <li><a href="Admin side/Loginside.php" class="button">Digital Print</a></li>
+            </div>
+            </div>
+      <div class="nav_dropdown">
+        <button class="btn_dropdown">Booking <i class="fas fa-chevron-down"></i></button>
+          <div class="dropdown-content">
+          <li><a href="Booking-system-Casper/index.php" class="button">Kalender</a></li>
+          <li><a href="Admin side/Loginside.php" class="button">Login</a></li>
+            </div>
+            </div>
+      <div class="nav_dropdown">
+        <button class="btn_dropdown">Kontakt <i class="fas fa-chevron-down"></i></button>
+          <div class="dropdown-content">
+          <li><a href="send_form_email.php" class="button">Kontakt</a></li>
+          <li><a href="send_form_email.php" class="button">FAQ</a></li>
+          <li><a href="send_form_email.php" class="button">Privatlivspolitik</a></li>
+          <li><a href="send_form_email.php" class="button">Vilkår & Betingelser</a></li>
+            </div>
+            </div>
+      <div class="nav_dropdown">
+        <button class="btn_dropdown"></button>
+          <div class="dropdown-content">
+          </div>
+      </div>
+    </ul>
+  </nav>
+
+
+  <!-- Heading -->
+<br> <br> <br>
+<section class="section_heading">
+	<div class="container u-full-width">
+		<h6><center>Kontaktinformationer
+		</center>	
+		</h6>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+</section>
+    
+<div>
+    
+<div class="kontaktinformation">
+    <div class="kontaktbillede">
+        <div class="kontaktikon"> 
+           <i class="fas fa-map-marker-alt"></i>     
+        </div>
+        
+        <div class="kontaktikon"> 
+            <i class="fas fa-phone"></i>
+        </div>
+    
+        <div class="kontaktikon"> 
+            <i class="fas fa-envelope"></i>
+        </div>
+    
+        <div class="kontaktikon"> 
+            <i class="fab fa-facebook-f"></i>
+        </div>
+    
+        <div class="kontaktikon"> 
+            <i class="fab fa-instagram"></i>
+        </div>
+    
+        <div class="kontaktikon"> 
+            <i class="fas fa-clock"></i>
+        </div>
+    </div>
+    <div>
+        <div class="informationer">
+            <div class="billedeinformation">
+            <div>
+            <p>Adresse: 
+                <br>Færøvej</p></div>
+            </div>
+    
+            <div class="billedeinformation">
+            <p>Telefonnummer:
+                <br>26834088</p>    
+            </div>
+    
+            <div class="billedeinformation">
+            <p><br>Email:
+                <br>Daniel_wow1@live.dk</p>
+            </div>
+    
+            <div class="billedeinformation">
+            <p><br>Facebook:
+                <br>Daniel Prior phorography</p>
+            </div>
+    
+            <div class="billedeinformation">
+            Instagram:
+            <p> Instagram:
+                <br>DaPrior</p>
+            </div>
+    
+            <div class="billedeinformation">
+            <p>Åbningstider:
+                <br>Alle ugens dage</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+    
+
+<div class="lokation"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2170.129099026749!2d9.917400615981306!3d57.049329380919936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4649328c9bc379a3%3A0x97ceb14dd16f56d2!2sBispensgade%2013%2C%209000%20Aalborg!5e0!3m2!1sda!2sdk!4v1582824343072!5m2!1sda!2sdk" width="500px" height="500" frameborder="0" style="border:0;" allowfullscreen=""></iframe></div>
+   
+</div>
+    
+    <br>
+   <!-- Top part of the footer-->
+  <section class="section_topfooter">
+    <div class="container">
+      <div class="u-full-width">
+        <div class="row">
+          <div class="six columns">
+            <center>
+            <p style="font-size: 16px">KONTAKTOPLYSNINGER</p>
+            <p style="font-size: 15px">Amalie Sandgaard<br>CVR: 38 02 70 34<br>+45 31 51 45 55<br>amaliesandgaardphotography@hotmail.com</p>
+            </center>
+          </div>
+          <div class="two columns">
+            <center>
+            <p style="font-size: 16px">NYTTIGE LINKS</p>
+            <a style="font-size: 15px" href="#" >Privatlivspolitik</a><br>
+            <a style="font-size: 15px" href="#">Vilkår & Betingelser</a>
+            </center>
+          </div>
+          <div class="two columns">
+            <center>
+            <p style="font-size: 16px">FØLG MIG PÅ</p>
+            <a href="https://www.facebook.com/amaliesandgaardphotography/" target="_blank"><i class="fab fa-facebook-square fa-2x"></i></a><br>
+            <a href="#" target="_blank"><i class="fab fa-instagram fa-2x"></i></a><br>
+            </center>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section> 
+ <!--Bottom part of the footer -->
+  <section class="section_bottomfooter">
+    <div>
+      <center>
+        <p style="font-size: 15px" class="footer"> &copy; Copyright <?php echo date("Y"); ?> | Amalie Sandgaard Photography | All Rights Reserved</p>
+      </center>
+    </div>
+  </section>
+</body>
+</html>
